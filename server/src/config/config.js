5879d6c6ced745df9a1db85408eab5eddd010034
DanielAdek <maildaniel.me1@gmail.com>
2018-08-24 19:19:27 +0100

    create Procfile and fix config errors

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -5,14 +5,14 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: "127.0.0.1",
+    host: process.env.DB_HOST,
     dialect: "postgres"
   },
   test: {
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: "127.0.0.1",
+    host: process.env.DB_HOST,
     dialect: "postgres"
   },
   production: {
